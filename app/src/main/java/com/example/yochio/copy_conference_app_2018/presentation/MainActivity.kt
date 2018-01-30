337cbef55b98f161fbb4b181eaaf551125f23359
yochidrop <miyazawa@nagisa-inc.jp>
2018-01-30 16:32:31 +0900

    とりあえず動くようにした。

--- app/src/main/java/com/example/yochio/copy_conference_app_2018/presentation/MainActivity.kt
+++ app/src/main/java/com/example/yochio/copy_conference_app_2018/presentation/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.yochio.copy_conference_app_2018.presentation
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.yochio.copy_conference_app_2018.R
+import com.example.yochio.copy_conference_app_2018.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+
     }
 
     companion object {
---
+++ app/src/main/java/com/example/yochio/copy_conference_app_2018/presentation/common/menu/DrawerMenu.kt
@@ -0,0 +1,11 @@
+package com.example.yochio.copy_conference_app_2018.presentation.common.menu
+
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import javax.inject.Inject
+
+/**
+ * Created by yochio on 2018/01/30.
+ */
